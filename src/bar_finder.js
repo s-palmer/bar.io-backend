54b9ce74af7c09b1ebce007e80d2692a71a3d739
MattW <mattnwarnock@yahoo.co.uk>
2021-12-05 18:38:35 +0000

    Refactor search

--- src/bar_finder.js
+++ src/bar_finder.js
@@ -2,29 +2,36 @@ class BarFinder {
   constructor(client, sorter) {
     this.client = client;
     this.sorter = sorter;
+    this.metersPerMinute = 80;
   }
 
   search(coordinates, walkTime) {
-    const metersPerMinute = 80,
-          walkTimeToMeters = walkTime * metersPerMinute;
-
-    let data = this.client.getLocations(coordinates, walkTimeToMeters),
+    let walkTimeToMeters = this.#convertToMeters(walkTime),
+        data = this.client.getLocations(coordinates, walkTimeToMeters),
         rawResults = this.sorter.topFiveBars(data);
 
-        return rawResults.map(bar => {
-          return {
-            name: bar.name,
-            formatted_address: bar.formatted_address,
-            location: {
-              lat: bar.geometry.location.lat,
-              lng: bar.geometry.location.lng
-            },
-            place_id: bar.place_id,
-            price_level: bar.price_level,
-            rating: bar.rating,
-            types: bar.types
-          }
-        });
+        return this.#format(rawResults);
+  }
+
+  #convertToMeters(walkTime) {
+    return walkTime * this.metersPerMinute;
+  }
+
+  #format(rawResults) {
+    return rawResults.map(bar => {
+      return {
+        name: bar.name,
+        formatted_address: bar.formatted_address,
+        location: {
+          lat: bar.geometry.location.lat,
+          lng: bar.geometry.location.lng
+        },
+        place_id: bar.place_id,
+        price_level: bar.price_level,
+        rating: bar.rating,
+        types: bar.types
+      }
+    });
   }
 }
 
