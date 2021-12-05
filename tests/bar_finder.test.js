a4a3ac81345c969ab0768dcb54f6265c46055010
MattW <mattnwarnock@yahoo.co.uk>
2021-12-05 17:03:15 +0000

    call sorter with results

--- tests/bar_finder.test.js
+++ tests/bar_finder.test.js
@@ -1,7 +1,8 @@
 const BarFinder = require('../src/bar_finder');
-const barResults = require('../fixtures/bar_results');
-const client = { getLocations() { return barResults } };
-const sorter = jest.fn();
+const clientResults = require('../fixtures/bar_results');
+const topFiveResults = require('../fixtures/top_five_bars')
+const client = { getLocations() { return clientResults } };
+const sorter = { topFiveBars() { return topFiveResults } };
 
 describe('BarFinder', () => {
   const barFinder = new BarFinder(client, sorter);
@@ -9,13 +10,23 @@ describe('BarFinder', () => {
   const walkTime = 15;
 
   describe('search', () => {
+    beforeEach(() => {
+      jest.spyOn(client, 'getLocations');
+      jest.spyOn(sorter, 'topFiveBars');
+    })
+
     it('calls the client with coordinates and meter radius', () => {
       const radiusInMeters = 1200;
-      jest.spyOn(client, 'getLocations');
 
       barFinder.search(coordinates, walkTime);
 
       expect(client.getLocations).toHaveBeenCalledWith(coordinates, radiusInMeters);
+    });
+
+    it('calls the sorter with client results', () => {
+      barFinder.search(coordinates, walkTime);
+
+      expect(sorter.topFiveBars).toHaveBeenCalledWith(clientResults);
     });
   });
 });
