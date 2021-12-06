f02d151e120fb0f2980eb1df62d6fc541e6e5b51
MattW <mattnwarnock@yahoo.co.uk>
2021-12-06 15:46:47 +0000

    Bars route to collect request body

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const app = express();
-const BarFinder = require('./bar_finder');
-const Client = require('./client');
-const Sorter = require('./sorter');
+      app = express(),
+      BarFinder = require('./bar_finder'),
+      Client = require('./client'),
+      Sorter = require('./sorter');
 
-app.get('/bars', async (req, res) => {
-  const barFinder = new BarFinder(new Client, new Sorter);
-  const coordinates = {x: 51.5173523, y: -0.0754469};
-  const walkTime = 15;
+app.use(express.json());
 
-  const results = await barFinder.search(coordinates, walkTime);
+app.post('/bars', async (req, res) => {
+  const barFinder = new BarFinder(new Client, new Sorter),
+        results = await barFinder.search(req.body.location, req.body.mins);
 
-  res.json({results: results});
+  res.send({results: results});
 });
 
 module.exports = app;
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -5,18 +5,27 @@ const formatted_response = require('../fixtures/formatted_response');
 const request = require('supertest');
 
 describe('/bars', () => {
+  const requestBody = {
+    mins: 15,
+    location: { lat: 51.5173523, lng: -0.0754469 }
+  };
+
   beforeEach(() => {
     jest.spyOn(Client.prototype, 'getLocations')
     .mockImplementation(() => clientResults.results);
 });
 
   it('returns 200 response', async () => {
-    await request(app).get('/bars').expect(200);
+    await request(app).post('/bars').expect(200);
   });
 
-  it('contains a json response', async () => {
-    const res = await request(app).get('/bars');
+  it('sends a json response of bars requested', async () => {
+    const res = await request(app)
+    .post('/bars')
+    .send(requestBody);
 
     expect(res.body).toEqual(formatted_response);
+    expect(Client.prototype.getLocations)
+    .toHaveBeenCalledWith(requestBody.location, 1200)
   });
 });
